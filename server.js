93028c3d0329773214f737c0e2ce4561b876887c
David Padilla <dpadman2@gmail.com>
2020-07-09 16:41:10 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const path = require('path');
 const mongoose = require("mongoose")
 
 const app = express()
@@ -7,7 +8,10 @@ mongoose.connect("mongodb://localhost/twilightApi", {userNewUrlParser:true})
 // settings
 
 
-app.use(express.static(__dirname + '/twilightFun/dist/twilightFun'))
+app.use(express.static(__dirname + '/twilightFun/dist/twilightFun'));
+app.get('/*',function(req,res){
+    res.sendFile(path.join(__dirname+'/twilightFun/dist/twilightFun/index.html'));
+});
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
