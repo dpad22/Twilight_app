eea0db86cbcc7df353aa84cc9ee0fbae9e30e85e
David Padilla <dpadman2@gmail.com>
2020-07-10 17:10:21 -0500

    Server.js update

--- server.js
+++ server.js
@@ -1,33 +1,16 @@
 const express = require("express")
-const path = require('path');
 const mongoose = require("mongoose")
 
 const app = express()
-// mongoose.connect("mongodb://localhost/twilightApi", {userNewUrlParser:true})
+mongoose.connect("mongodb://localhost/twilightApi", {userNewUrlParser:true})
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/twilightApi";
-
-const options = {
-    userNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    
-};
-
-mongoose.connect(MONGODB_URI, options)
-// settings
+app.use(express.static(__dirname + '/twilightFun/dist/twilightFun'));
 
 
-app.use(express.static(__dirname + '/twilight-fun/dist/twilight-fun'));
-app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+ '/dist//twilight-fun/index.html'));
-});
-
-
-// app.use(express.json())
+app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 
 require('./server/config/routes.js')(app);
 
 
-app.listen(process.env.PORT || 1337, () => console.log("listening on port 1337. Twilight App!"));
+app.listen(1337, () => console.log("listening on port 1337. Twilight App!"));
