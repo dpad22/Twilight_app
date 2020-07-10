72336de1c8a6a818dc1ce66acfab545bfdcaf9fd
David Padilla <dpadman2@gmail.com>
2020-07-10 15:31:22 -0500

    test

--- server.js
+++ server.js
@@ -3,15 +3,28 @@ const path = require('path');
 const mongoose = require("mongoose")
 
 const app = express()
-mongoose.connect("mongodb://localhost/twilightApi", {userNewUrlParser:true})
+// mongoose.connect("mongodb://localhost/twilightApi", {userNewUrlParser:true})
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/twilightApi";
+
+const options = {
+    userNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
+};
+
+mongoose.connect(MONGODB_URI, options)
 // settings
 
 
-app.use(express.static(__dirname + '/dist/twilight-fun'));
+app.use(express.static(__dirname + '/twilight-fun/dist/twilight-fun'));
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+ '/dist/twilight-fun/index.html'));
+    res.sendFile(path.join(__dirname+ '/dist/index.html'));
 });
+
+db = client.db()
+console.log("Database connection ready");
 
 // app.use(express.json())
 app.use(express.urlencoded({extended:true}));
